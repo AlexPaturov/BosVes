f7fd042c204b2d0716f830967e6797eb0bd2c085
AlexPaturov <alexey.paturov@gmail.com>
2024-07-09 11:40:19 +0300

    Добавил js для отображения размера окна.

---
+++ BosVesUI/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+
+
+// размер окна 
+window.getWindowSize = () => {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight
+    };
+};
