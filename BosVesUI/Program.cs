b071234e6a50970ee3c6960d4cd687a79767be6f
AlexPaturov <alexey.paturov@gmail.com>
2024-11-10 07:58:23 +0200

    Сделал формат даты dd.MM.yyyy и времени HH:mm:ss для всего приложения.

--- BosVesUI/Program.cs
+++ BosVesUI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.ApplicationInsights.Extensibility;
+using System.Globalization;
 
 namespace BosVesUI
 {
@@ -10,34 +11,41 @@ namespace BosVesUI
          try
          {
             var builder = WebApplication.CreateBuilder(args);
-
             builder.Logging.ClearProviders();  // Remove other logging providers if you want only NLog
             builder.ConfigureServices();
-
+            #region Set up the culture to "dd.MM.yyyy"
+            var cultureInfo = new CultureInfo("en-GB") // 'en-GB' uses 'dd.MM.yyyy' format by default
+            {
+               DateTimeFormat = { ShortDatePattern = "dd.MM.yyyy", LongDatePattern = "dd.MM.yyyy HH:mm:ss" }
+            };
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+            #endregion
             var app = builder.Build();
-
+            #region Middleware setup to the culture to "dd.MM.yyyy"
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+               DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture(cultureInfo),
+               SupportedCultures = new List<CultureInfo> { cultureInfo },
+               SupportedUICultures = new List<CultureInfo> { cultureInfo }
+            });
+            #endregion
+            
             // Step 3: Log information (example)
             //logger.Info("Blazor app started!");
-           
+
             app.UseHsts();
             app.UseExceptionHandler("/Error");
             app.MapControllers();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
-
-
-       
-
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
-
 
             // Step 4: Log a message during app lifetime events (optional)
             //app.Lifetime.ApplicationStarted.Register(() => logger.Info("Application has started."));
             //app.Lifetime.ApplicationStopped.Register(() => logger.Info("Application has stopped."));
-
-
             app.Run();
          }
          catch (Exception exception)
--- BosVesUI/RegisterServices.cs
+++ BosVesUI/RegisterServices.cs
@@ -7,41 +7,20 @@ public static class RegisterServices
 {
    public static void ConfigureServices(this WebApplicationBuilder builder) 
    {
-      builder.Services.Configure<RequestLocalizationOptions>(options =>
-      {
-         var supportedCultures = new[] { "en-US", "ru", "uk" };
-         options.SetDefaultCulture(supportedCultures[0])
-             .AddSupportedCultures(supportedCultures)
-             .AddSupportedUICultures(supportedCultures);
-      });
-
       builder.Services.AddRazorPages();
       builder.Services.AddServerSideBlazor();
       builder.Services.AddControllers();
-
-      //-------------------------работаю с логами--------------------------------
       builder.Logging.ClearProviders();
       builder.Logging.AddConsole();
       builder.Logging.AddApplicationInsights();
       builder.Services.AddApplicationInsightsTelemetry();
-
-      // Register IHttpContextAccessor
-      builder.Services.AddHttpClient();
       builder.Services.AddHttpContextAccessor();
-
-      // Register TelemetryService
-      //builder.Services.AddSingleton<TelemetryService>();
-      //---------------------------------------------------------
-
       builder.Services.AddScoped<CircuitHandler, CustomCircuitHandler>();
-
       builder.Services.AddMemoryCache();
-      // ------------------------------------------------------------------------------------------------------------------------
       builder.Services.AddSingleton<XmlDecodeService>();  // служба хмл - декодер
       builder.Services.AddSingleton<UserService>();       // служба служит обработчиком и хранилищем информации о пользователе
       builder.Services.AddSingleton<TcpClientService>();  // служба получающая имя пк юзера по TCP от службы windows
       builder.Services.AddHostedService<CommandService>();// 
-      //-------------------------------------------------------------------------------------------------------------------------
       builder.Services.Configure<ActiveDirectoryHelper>(builder.Configuration.GetSection("ActiveDirectory"));
       builder.Services.Configure<BosVesAppSettings>(builder.Configuration.GetSection("BosVesAppSettings")); // заполняю конфигурационными настройками классс, который планирую использовать во всём приложении
       builder.Services.AddSingleton<BosVesAppSettings>();            // 
